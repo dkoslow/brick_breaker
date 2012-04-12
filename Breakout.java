b377db91516caa759f21087cd7fa546f0d6f81c4
S <s>
2012-04-12 13:25:58 -0400

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -113,6 +113,10 @@ public class Breakout extends GraphicsProgram {
 				if ((getX() - lastX) > 0) {
 					paddleMovement = (getX() - lastX);
 				}
+			} else if(lastX + PADDLE_WIDTH == getWidth()) {
+				if (getX() - lastX < 0 ) {
+					
+				}
 			}
 			paddleMovement = (getX() - lastX);
 				
