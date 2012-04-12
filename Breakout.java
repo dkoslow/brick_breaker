6e67198cb43cbc8fed5d49a0f842750095700400
S <s>
2012-04-12 13:04:38 -0400

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -111,7 +111,7 @@ public class Breakout extends GraphicsProgram {
 		lastY = paddle.getY();
 		if( paddle != null ) {
 			if (not touching wall){
-				paddleMovement = 2.0;
+				paddleMovement = (getX() - lastX());
 			}
 			paddle.move(paddleMovement, 0);
 			lastX = e.getX();
