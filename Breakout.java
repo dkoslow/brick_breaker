e4b58d0b29819be00eab0f27140963c3482cc9cf
S <s>
2012-04-12 00:30:25 -0400

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -110,6 +110,10 @@ public class Breakout extends GraphicsProgram {
 		vy = 3.0;
 		vx = rgen.nextDouble(1.0,3.0);
 		if (rgen.nextBoolean(0.5)) vx = -vx;
+		while(true) {
+			ball.move(vx, vy);
+			pause(50);
+		}
 		
 		
 		
