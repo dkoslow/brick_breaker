d0e3a1b1b1593db4350c4574658e2cbecdd120b7
S <s>
2012-04-13 13:15:27 -0400

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -65,7 +65,7 @@ public class Breakout extends GraphicsProgram {
 	public void run() {
 		createBricks();
 		createPaddle();
-		GLabel lifeCount = new GLabel("Lives: " + lives + " left",10 ,10 );
+		GLabel lifeCount = new GLabel("Lives: " + lives + " left", 10 ,10 );
 		lifeCount.setFont("Times-12");
 		add(lifeCount);
 		addMouseListeners();
